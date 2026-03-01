c2a8d1cd2dbbd1f915f148ca24d9e744d63fc15a
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-01 21:21:34 +0500

    Update BusStrategy.java

--- src/strategy/strategies/BusStrategy.java
+++ src/strategy/strategies/BusStrategy.java
@@ -1,4 +1,18 @@
-package strategy.strategies;
+package org.example.strategy.strategies;
 
-public class BusStrategy {
+import org.example.strategy.ICostCalculationStrategy;
+import org.example.strategy.model.*;
+
+public class BusStrategy implements ICostCalculationStrategy {
+
+    @Override
+    public double calculate(TripRequest r) {
+        double base = r.distance * 6;
+
+        if (r.luggage) base += 1000;
+        if (r.childDiscount) base *= 0.7;
+        if (r.pensionerDiscount) base *= 0.8;
+
+        return base * r.passengers;
+    }
 }
