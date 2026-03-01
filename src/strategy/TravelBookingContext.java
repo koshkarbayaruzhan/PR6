9148debf7d21937c0d32792e48f75fd2f8656593
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-01 21:19:39 +0500

    Update TravelBookingContext.java

--- src/strategy/TravelBookingContext.java
+++ src/strategy/TravelBookingContext.java
@@ -1,4 +1,19 @@
-package strategy;
+package org.example.strategy;
+
+import org.example.strategy.model.TripRequest;
 
 public class TravelBookingContext {
+
+    private ICostCalculationStrategy strategy;
+
+    public void setStrategy(ICostCalculationStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public double calculatePrice(TripRequest request) {
+        if (strategy == null) {
+            throw new IllegalStateException("Стратегия не выбрана");
+        }
+        return strategy.calculate(request);
+    }
 }
