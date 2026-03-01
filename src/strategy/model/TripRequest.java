2749f55ec0e1e7d6d92f6f286973ad442e5f6273
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-01 21:20:19 +0500

    Update TripRequest.java

--- src/strategy/model/TripRequest.java
+++ src/strategy/model/TripRequest.java
@@ -1,4 +1,26 @@
-package strategy.model;
+package org.example.strategy.model;
 
 public class TripRequest {
+
+    public double distance;
+    public ServiceClass serviceClass;
+    public int passengers;
+    public boolean childDiscount;
+    public boolean pensionerDiscount;
+    public boolean luggage;
+
+    public TripRequest(double distance,
+                       ServiceClass serviceClass,
+                       int passengers,
+                       boolean childDiscount,
+                       boolean pensionerDiscount,
+                       boolean luggage) {
+
+        this.distance = distance;
+        this.serviceClass = serviceClass;
+        this.passengers = passengers;
+        this.childDiscount = childDiscount;
+        this.pensionerDiscount = pensionerDiscount;
+        this.luggage = luggage;
+    }
 }
