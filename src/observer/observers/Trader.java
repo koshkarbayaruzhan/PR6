15e41b2724e8e4548b94c8d33dc4f431abeb7041
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-01 21:23:04 +0500

    Update Trader.java

--- src/observer/observers/Trader.java
+++ src/observer/observers/Trader.java
@@ -1,4 +1,22 @@
-package observer.observers;
+package org.example.observer.observers;
 
-public class Trader {
+import org.example.observer.IObserver;
+
+public class Trader implements IObserver {
+
+    private final String name;
+
+    public Trader(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void update(String stock, double price) {
+        System.out.println("Трейдер " + name + " получил: " + stock + " = " + price);
+    }
+
+    @Override
+    public String toString() {
+        return "Trader[" + name + "]";
+    }
 }
