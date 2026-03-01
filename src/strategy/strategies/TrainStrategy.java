b698d03d51a33a05e63105dd72479aa88bb2b2f0
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-01 21:21:19 +0500

    Update TrainStrategy.java

--- src/strategy/strategies/TrainStrategy.java
+++ src/strategy/strategies/TrainStrategy.java
@@ -1,4 +1,19 @@
-package strategy.strategies;
+package org.example.strategy.strategies;
 
-public class TrainStrategy {
+import org.example.strategy.ICostCalculationStrategy;
+import org.example.strategy.model.*;
+
+public class TrainStrategy implements ICostCalculationStrategy {
+
+    @Override
+    public double calculate(TripRequest r) {
+        double base = r.distance * 12;
+
+        if (r.serviceClass == ServiceClass.BUSINESS) base *= 1.3;
+        if (r.luggage) base += 2000;
+        if (r.childDiscount) base *= 0.6;
+        if (r.pensionerDiscount) base *= 0.75;
+
+        return base * r.passengers;
+    }
 }
