60e784b6607caa7c1f1afb32c468c3c88bfed540
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-01 21:21:01 +0500

    Update PlaneStrategy.java

--- src/strategy/strategies/PlaneStrategy.java
+++ src/strategy/strategies/PlaneStrategy.java
@@ -1,4 +1,19 @@
-package strategy.strategies;
+package org.example.strategy.strategies;
 
-public class PlaneStrategy {
+import org.example.strategy.ICostCalculationStrategy;
+import org.example.strategy.model.*;
+
+public class PlaneStrategy implements ICostCalculationStrategy {
+
+    @Override
+    public double calculate(TripRequest r) {
+        double base = r.distance * 25;
+
+        if (r.serviceClass == ServiceClass.BUSINESS) base *= 1.6;
+        if (r.luggage) base += 5000;
+        if (r.childDiscount) base *= 0.5;
+        if (r.pensionerDiscount) base *= 0.7;
+
+        return base * r.passengers;
+    }
 }
