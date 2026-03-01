ace710d25fded183bbeb75f484660b08cbf7eea4
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-01 21:22:39 +0500

    Update StockExchange.java

--- src/observer/model/StockExchange.java
+++ src/observer/model/StockExchange.java
@@ -1,4 +1,37 @@
-package observer.model;
+package org.example.observer.model;
 
-public class StockExchange {
+import org.example.observer.*;
+
+import java.util.*;
+
+public class StockExchange implements ISubject {
+
+    private final Map<String, List<IObserver>> observers = new HashMap<>();
+
+    @Override
+    public void subscribe(String stock, IObserver observer) {
+        observers.computeIfAbsent(stock, k -> new ArrayList<>()).add(observer);
+        System.out.println(observer + " подписался на " + stock);
+    }
+
+    @Override
+    public void unsubscribe(String stock, IObserver observer) {
+        if (observers.containsKey(stock)) {
+            observers.get(stock).remove(observer);
+        }
+    }
+
+    @Override
+    public void notifyObservers(String stock, double price) {
+        if (!observers.containsKey(stock)) return;
+
+        for (IObserver o : observers.get(stock)) {
+            o.update(stock, price);
+        }
+    }
+
+    public void updatePrice(String stock, double price) {
+        System.out.println("\nБиржа: " + stock + " = " + price);
+        notifyObservers(stock, price);
+    }
 }
