5395c25492f9aca9e963682e3e8fa97ada611c6a
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-01 21:23:26 +0500

    Update TradingBot.java

--- src/observer/observers/TradingBot.java
+++ src/observer/observers/TradingBot.java
@@ -1,4 +1,25 @@
-package observer.observers;
+package org.example.observer.observers;
 
-public class TradingBot {
+import org.example.observer.IObserver;
+
+public class TradingBot implements IObserver {
+
+    private final double buyLimit;
+
+    public TradingBot(double buyLimit) {
+        this.buyLimit = buyLimit;
+    }
+
+    @Override
+    public void update(String stock, double price) {
+        if (price < buyLimit)
+            System.out.println("BOT: покупаю " + stock + " по " + price);
+        else
+            System.out.println("BOT: пропускаю " + stock);
+    }
+
+    @Override
+    public String toString() {
+        return "TradingBot";
+    }
 }
